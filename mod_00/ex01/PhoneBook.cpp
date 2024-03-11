75a68a21646a64f7a51a4903ff65b34e052b025c
gbreana <gbreana@student.42.fr>
2024-03-11 17:34:39 +0100

    some variant of add command

--- mod_00/ex01/PhoneBook.cpp
+++ mod_00/ex01/PhoneBook.cpp
@@ -1,5 +1,6 @@
 #include "includes/PhoneBook.hpp"
 #include <iostream>
+#include <iomanip>
 
 
 PhoneBook::PhoneBook(): currentSize(0) {
@@ -34,19 +35,41 @@ void PhoneBook::searchContact() {
 }
 
 void PhoneBook::displayContacts() {
-	std::cout << "-------------------------------------------" << std::endl;
+	std::cout << "---------------------------------------------" << std::endl;
 	std::cout << "|";
-	std::cout << "     Index" << "|";
-	std::cout << " FirstName" << "|";
-	std::cout << "  LastName" << "|";
-	std::cout << "  Nickname" << "|" << std::endl;
-	std::cout << "-------------------------------------------" << std::endl;
+	std::cout << std::setw(10) << "Index" << "|";
+	std::cout << std::setw(10) << "FirstName" << "|";
+	std::cout << std::setw(10) << "LastName" << "|";
+	std::cout << std::setw(10) << "Nickname" << "|" << std::endl;
+	std::cout << "---------------------------------------------" << std::endl;
 
 	for (int index = 0; index < maxSize; ++index)
 	{
-		std::cout << "|" << index << "|";
+		std::cout << "|" << std::setw(10) << index << "|";
+
+
+
+		std::string firstname = contacts[index].getFirstName();
+		std::string lastname = contacts[index].getLastName();
+		std::string nickname = contacts[index].getNickname();
+
+		if (firstname.length() > 10) {
+			firstname = firstname.substr(0, 9) + ".";
+		}
+
+		if (lastname.length() > 10) {
+			lastname = lastname.substr(0, 9) + ".";
+		}
+
+		if (nickname.length() > 10) {
+			nickname = nickname.substr(0, 9) + ".";
+		}
+
+		std::cout << std::setw(10) << firstname << "|";
+		std::cout << std::setw(10) << lastname << "|";
+		std::cout << std::setw(10) << nickname << "|";
 		std::cout << std::endl;
-		std::cout << "-------------------------------------------" << std::endl;
+		std::cout << "---------------------------------------------" << std::endl;
 
 	}
 }
--- mod_00/ex01/main.cpp
+++ mod_00/ex01/main.cpp
@@ -1,29 +1,46 @@
 #include <iostream>
+#include <iomanip>
 #include "includes/PhoneBook.hpp"
 
 int main()
 {
-	using std::string;
-	using std::cin;
-
-
-	string firstname;
-	string lastname;
-	string nickname;
-	string phonenumber;
-	string darkestsecret;
-
+	int flag = 1;
+	std::string prompt;
 	PhoneBook pb;
-/*
-	cin >> firstname;
-	cin >> lastname;
-	cin >> nickname;
-	cin >> phonenumber;
-	cin >> darkestsecret;
+	//pb.searchContact();
 
-	pb.addContact(firstname, lastname, nickname, phonenumber, darkestsecret);*/
+	while(flag) {
+		std::cout << "Enter command: ";
+		std::cin >> prompt;
+		std::cout << std::endl;
+		std::cout << prompt << std::endl;
+		if (prompt == "EXIT") {
+			flag = 0;
+		} else if (prompt == "SEARCH") {
+			pb.searchContact();
+		} else if (prompt == "ADD") {
+			std::string firstname;
+			std::cout << std::setw(15) << "First Name: ";
+			std::cin >> firstname;
+			std::string lastname;
+			std::cout << std::setw(15) << "Last Name: ";
+			std::cin >> lastname;
+			std::string nickname;
+			std::cout << std::setw(15) << "Nickname: ";
+			std::cin >> nickname;
+			std::string phonenumber;
+			std::cout << std::setw(15) << "Phone Number: ";
+			std::cin >> phonenumber;
+			std::string dearkestsecret;
+			std::cout << std::setw(15) << "Darkest Secret: ";
+			std::cin >> dearkestsecret;
 
-	pb.searchContact();
-
+			pb.addContact(firstname, \
+							lastname, \
+							nickname, \
+							phonenumber, \
+							dearkestsecret);
+		}
+	}
 	return 0;
 }
