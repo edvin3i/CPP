73394791eefeaaa8bdbb3ded1dafe81b38743d05
edvin3i <edviniii@gmail.com>
2024-03-11 05:35:22 +0100

    add some code in main

--- mod_00/ex01/main.cpp
+++ mod_00/ex01/main.cpp
@@ -1,6 +1,26 @@
-#include "PhoneBook.cpp"
+#include <iostream>
+#include "includes/PhoneBook.hpp"
 
 int main()
 {
+	using std::string;
+	using std::cin;
+
+
+	string firstname;
+	string lastname;
+	string nickname;
+	string phonenumber;
+	string darkestsecret;
+
+	PhoneBook pb;
+
+	cin >> firstname;
+	cin >> lastname;
+	cin >> nickname;
+	cin >> phonenumber;
+	cin >> darkestsecret;
+
+	pb.addContact(firstname, lastname, nickname, phonenumber, darkestsecret);
 	return 0;
 }
