17d5e1fd1102af1608ff837e495852963ddb14a3
edvin3i <edviniii@gmail.com>
2024-03-12 18:58:05 +0100

    codmetic changes

--- mod_00/ex01/main.cpp
+++ mod_00/ex01/main.cpp
@@ -1,21 +1,18 @@
 #include <iostream>
-#include <iomanip>
 #include "includes/PhoneBook.hpp"
 
 int main()
 {
-	int flag = 1;
+	bool flag = true;
 	std::string prompt;
 	PhoneBook pb;
-	//pb.searchContact();
 
 	while(flag) {
 		std::cout << "Enter command: ";
 		std::cin >> prompt;
-		std::cout << std::endl;
 		std::cout << prompt << std::endl;
 		if (prompt == "EXIT") {
-			flag = 0;
+			flag = false;
 		} else if (prompt == "SEARCH") {
 			pb.searchContact();
 		} else if (prompt == "ADD") {
