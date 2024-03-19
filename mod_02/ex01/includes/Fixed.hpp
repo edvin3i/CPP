161caecafcd4080d99fe3dc372f5d53dffdec46c
edvin3i <edviniii@gmail.com>
2024-03-19 09:20:25 +0100

    mod01 ex01 started

---
+++ mod_02/ex01/includes/Fixed.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#ifndef EX00_FIXED_HPP
+#define EX00_FIXED_HPP
+
+#include <iostream>
+
+class Fixed {
+
+public:
+	// default constructor
+	Fixed();
+	// copy constructor
+	Fixed(const Fixed &other_obj);
+	// copy assignment operator overload
+	Fixed &operator=(const Fixed &other_obj);
+	// destructor
+	~Fixed();
+	// getter
+	int getRawBits(void) const;
+	// setter
+	void setRawBits(int const raw);
+
+private:
+	int num_val;
+	static const int bits = 8;
+};
+
+
+#endif
