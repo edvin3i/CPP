62c37fa035c8dc181aad046008b424442e2ec61b
edvin3i <edviniii@gmail.com>
2024-03-25 23:11:38 +0100

    Added DiamondTrap class prototypes

--- mod_03/ex03/includes/DiamondTrap.hpp
+++ mod_03/ex03/includes/DiamondTrap.hpp
@@ -4,7 +4,7 @@
 #include "ScavTrap.hpp"
 #include "FragTrap.hpp"
 
-class DiamondTrap : virtual ScavTrap, virtual FragTrap {
+class DiamondTrap : public ScavTrap, public FragTrap {
 
 public:
 	DiamondTrap(void);
