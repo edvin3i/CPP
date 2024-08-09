f1380987c292981617e40fa63927cfc8e73ce621
gbreana <gbreana@student.42.fr>
2024-08-09 19:38:46 +0200

    ex02 is complete

--- mod_06/ex02/main.cpp
+++ mod_06/ex02/main.cpp
@@ -32,58 +32,46 @@ void identify(Base *p) {
 }
 
 void identify(Base &p) {
-	if (dynamic_cast<A*>(&p))
+	try {
+		A& a = dynamic_cast<A&>(p);
 		std::cout << "Reference to A class instance" << std::endl;
-	else if (dynamic_cast<B*>(&p))
+		(void)a;
+		return ;
+	}
+	catch(...) {
+	}
+	try {
+		B& b = dynamic_cast<B&>(p);
 		std::cout << "Reference to B class instance" << std::endl;
-	else if (dynamic_cast<C*>(&p))
+		(void)b;
+		return ;
+	}
+	catch(...) {
+	}
+	try {
+		C& c = dynamic_cast<C&>(p);
 		std::cout << "Reference to C class instance" << std::endl;
-	else
-		std::cout << "Unknown type." << std::endl;
-
+		(void)c;
+		return ;
+	}
+	catch(...) {
+	}
 }
 
 int main() {
 
-	std::cout << BG_BRIGHT_BLACK"Test one."RESET << std::endl;
-	std::cout << BRIGHT_BLUE"Creating a random class (A, B or C)"RESET << std::endl;
+	std::cout << BG_BRIGHT_BLACK"Test output."RESET << std::endl;
+	std::cout << "\n" << BRIGHT_BLUE"Creating a random class (A, B or C)"RESET"\n" << std::endl;
 	Base *class_one = generate();
 
 	std::cout << BRIGHT_MAGENTA"Identifying by pointer:"RESET << std::endl;
 	identify(class_one);
 
-	std::cout << BRIGHT_YELLOW"Identifying by reference:"RESET << std::endl;
+	std::cout << "\n" << BRIGHT_YELLOW"Identifying by reference:"RESET << std::endl;
 	identify(*class_one);
 
-	std::cout << RED"Destruct the class.\n"RESET << std::endl;
+	std::cout << "\n" << RED"Destruct the class.\n"RESET << std::endl;
 	delete class_one;
 
-
-	std::cout << BG_BRIGHT_BLACK"Test two."RESET << std::endl;
-	std::cout << BRIGHT_BLUE"Creating a random class (A, B or C)"RESET << std::endl;
-	Base *class_two = generate();
-
-	std::cout << BRIGHT_MAGENTA"Identifying by pointer:"RESET << std::endl;
-	identify(class_two);
-
-	std::cout << BRIGHT_YELLOW"Identifying by reference:"RESET << std::endl;
-	identify(*class_two);
-
-	std::cout << RED"Destruct the class.\n"RESET << std::endl;
-	delete class_two;
-
-
-	std::cout << BG_BRIGHT_BLACK"Test two."RESET << std::endl;
-	std::cout << BRIGHT_BLUE"Creating a random class (A, B or C)"RESET << std::endl;
-	Base *class_three = generate();
-
-	std::cout << BRIGHT_MAGENTA"Identifying by pointer:"RESET << std::endl;
-	identify(class_three);
-
-	std::cout << BRIGHT_YELLOW"Identifying by reference:"RESET << std::endl;
-	identify(*class_three);
-
-	std::cout << RED"Destruct the class.\n"RESET << std::endl;
-	delete class_three;
 	return (0);
 }
