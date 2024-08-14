57ef2ffd14cb30ec8c555dc3c097f4494a5de549
gbreana <gbreana@student.42.fr>
2024-08-14 20:47:48 +0200

    creating easyfind

--- mod_08/ex00/includes/easyfind.hpp
+++ mod_08/ex00/includes/easyfind.hpp
@@ -1,4 +1,14 @@
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
+#include <iostream>
+#include <vector>
+
+template <typename T>
+T easyfind(T &cont, int &val);
+
+
+
+#include "easyfind.tpp"
+
 #endif
---
+++ mod_08/ex00/includes/easyfind.tpp
@@ -0,0 +1,4 @@
+template<typename T>
+T easyfind(T &cont, int &val) {
+	return NULL;
+}
