f13620f34a7148595be7193a13581fc428f9cf07
edvin3i <edviniii@gmail.com>
2024-08-16 14:35:16 +0200

    added MutantStack class

---
+++ mod_08/ex02/main.cpp
@@ -0,0 +1,23 @@
+#include "MutantStack.hpp"
+
+
+void test_big() {
+
+	std::cout << "\n" << BG_BRIGHT_BLACK"1. My test with big size of span:"RESET << std::endl;
+
+}
+
+
+void test_subject() {
+	std::cout << "\n" << BG_BRIGHT_BLACK"2. Standard small test from the subject:"RESET << std::endl;
+
+}
+
+int main() {
+
+	test_big();
+
+	test_subject();
+
+	return (0);
+}
