0377d7d89d53555e8fa189d42b2aa96e7b4f8945
edvin3i <edviniii@gmail.com>
2024-08-24 06:36:33 +0200

    db file loader added

--- mod_09/ex00/BitcoinExchange.cpp
+++ mod_09/ex00/BitcoinExchange.cpp
@@ -1,1 +1,43 @@
 #include "includes/BitcoinExchange.hpp"
+
+BitcoinExchange::BitcoinExchange(std::string &fileName) {
+	this->loadDatabase(fileName);
+}
+
+void BitcoinExchange::handleInputFile(const std::string &filename) const {
+
+}
+
+BitcoinExchange::~BitcoinExchange() {
+
+}
+
+void BitcoinExchange::loadDatabase(const std::string &filename) {
+	std::ifstream file(filename);
+	if (!file.is_open()) {
+		throw std::runtime_error("Error: Could not open file!");
+	}
+
+	std::string line;
+	while(std::getline(file, line)) {
+		std::stringstream ss(line);
+		std::string date;
+		std::string strRate;
+		double rate;
+
+		if(std::getline(ss, date, ',') && std::getline(ss, strRate)) {
+			try {
+				rate = std::stod(strRate);
+				_inMemoryDB[date] = rate;
+			}
+			catch(const std::exception &e) {
+				std::cerr << BRIGHT_RED"Error: Invalid data format in the line: "RESET << line << std::endl;
+			}
+		}
+		else {
+			std::cerr << BRIGHT_RED"Error: Could not parse the line!"RESET << line << std::endl;
+		}
+	}
+	file.close();
+}
+
