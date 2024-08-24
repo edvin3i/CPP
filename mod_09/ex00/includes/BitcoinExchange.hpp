4d219733cb98e019a5af7b4a8ce6da1213231ccc
edvin3i <edviniii@gmail.com>
2024-08-24 06:02:53 +0200

    add methods to btc class

--- mod_09/ex00/includes/BitcoinExchange.hpp
+++ mod_09/ex00/includes/BitcoinExchange.hpp
@@ -1,13 +1,26 @@
 #ifndef BITCOINEXCHANGE_HPP
 #define BITCOINEXCHANGE_HPP
 
+#include <string>
 #include <iostream>
 #include <fstream>
+#include <map>
 #include <exception>
 
 class BitcoinExchange {
-	BitcoinExchange();
+public:
+	BitcoinExchange(std::string fileName);
+	void handleInputFile(const std::string &filename) const;
 	~BitcoinExchange();
+
+private:
+	std::map<std::string, double> _inMemoryDB;
+
+	void loadDatabase(const std::string &filename);
+	double getExchangeRate(const std::string &date) const;
+	bool isValidDate(const std::string &date) const;
+	bool isValidValue(const std::string &value) const;
+
 
 };
 
