e43b679c27c68f4c0f35dd8ad2ae2307ea0d29e9
gbreana <gbreana@student.42.fr>
2024-08-26 23:34:52 +0200

    isValidDate complete

--- mod_09/ex00/BitcoinExchange.cpp
+++ mod_09/ex00/BitcoinExchange.cpp
@@ -123,5 +123,27 @@ bool BitcoinExchange::isValidDate(const std::string &date) const {
 		return (false);
 	}
 
+	int daysInMonth;
+	switch (month) {
+		case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+			daysInMonth = 31;
+
+		case 4: case 6: case 9: case 11:
+			daysInMonth = 30;
+
+		case 2:
+			if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+				daysInMonth = 29;
+			}
+			else {
+				daysInMonth = 28;
+			}
+	}
+
+	if (day < 1 || day > daysInMonth) {
+		return (false);
+	}
+
+	return (true);
 }
 
